4ddacfe9aa6a19099afb637335bec037c446c717
anyone-hub <slinkiesviews@gmail.com>
2026-08-04 01:19:45 -0500

    initial commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-gradle-plugin`
-    kotlin("jvm") version "1.9.22"
-    id("com.vanniktech.maven.publish") version "0.28.0"
+    kotlin("jvm") version "2.4.10"
+    id("com.vanniktech.maven.publish")  version "0.37.0"
 }
 
 repositories {
@@ -22,44 +22,40 @@ gradlePlugin {
     }
 }
 
-import com.vanniktech.maven.publish.SonatypeHost
+group = "com.anyonehub.diagnostics"
+version = "1.0.0"
 
 mavenPublishing {
-    coordinates(
-        groupId = "com.anyonehub.diagnostics",
-        artifactId = "health",
-        version = "1.0.0"
-    )
+    publishToMavenCentral()
+    signAllPublications()
 
-    pom {
-        name.set("Anyone-Hub Project Health Plugin")
-        description.set("A headless, read-only Gradle diagnostic plugin outputting Markdown reports.")
-        inceptionYear.set("2024")
-        url.set("https://github.com/anyonehub-dev/anyonehub-build-diagnostics-gradle-plugin")
-
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-
-        developers {
-            developer {
-                id.set("anyone-Hub")
-                name.set("Thomas Alan Slinkard Jr")
-                url.set("https://github.com/anyonehub-dev")
-            }
-        }
-
-        scm {
+        pom {
+            name.set("Anyone-Hub Project Health Plugin")
+            description.set("A headless, read-only Gradle diagnostic plugin outputting Markdown reports.")
+            inceptionYear.set("2024")
             url.set("https://github.com/anyonehub-dev/anyonehub-build-diagnostics-gradle-plugin")
-            connection.set("scm:git:git://github.com/anyonehub-dev/anyonehub-build-diagnostics-gradle-plugin.git")
-            developerConnection.set("scm:git:ssh://git@github.com/anyonehub-dev/anyonehub-build-diagnostics-gradle-plugin.git")
+
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("anyone-Hub")
+                    name.set("Thomas Alan Slinkard Jr")
+                    url.set("https://github.com/anyonehub-dev")
+                }
+            }
+
+            scm {
+                url.set("https://github.com/anyonehub-dev/anyonehub-build-diagnostics-gradle-plugin")
+                connection.set("scm:git:git://github.com/anyonehub-dev/anyonehub-build-diagnostics-gradle-plugin.git")
+                developerConnection.set("scm:git:ssh://git@github.com/anyonehub-dev/anyonehub-build-diagnostics-gradle-plugin.git")
+            }
         }
     }
 
-    signAllPublications()
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-}
